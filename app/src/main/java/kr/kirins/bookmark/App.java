9d545638d9750522b7a832f8a8075721b35d8b4f
daem0n <daem0n@kidsnote.com>
2020-01-17 01:08:49 +0900

    no message

---
+++ app/src/main/java/kr/kirins/bookmark/App.java
@@ -0,0 +1,14 @@
+package kr.kirins.bookmark;
+
+import android.app.Application;
+
+/**
+ * Created By sinhyeongseob on 2020-01-17
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
